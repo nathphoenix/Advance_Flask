7041a8c485fd2abb9e1db87235cc27ee40dc0386
nathphoenix <nathphoenix75@gmail.com>
2019-08-07 19:14:11 +0100

    Api completed

--- code/app.py
+++ code/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
-
+from flask import Flask
+from flask_restful import Api 
+from flask_jwt import JWT
+from resources.item import Item, ItemList  #we create a resource folder with __init__.py whch makes it a package and not an ordinary folder
+from resources.store import Store, StoreList
 from security import authenticate, identity
+from resources.user import UserRegister
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///data.db"   #in order to loacte our db file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #this modifies the tracker fro proper modification when changes occur and not save to alchemy
 app.secret_key = "Jose"
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 # class Student(Resource):
 #   def get(self, name):
@@ -14,56 +26,17 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)  #/auth
 
 # api.add_resource(Student, "/student/<string:name>")
-items = []
+# items = []
 
-class Item(Resource):
-  parser = reqparse.RequestParser()
-  parser.add_argument("price", type=float, required=True, help="Price Cannot be empty")
 
-  @jwt_required() #this is a decorator, we can use this on any of our method that we want to add
-  #authentication to before the method will run, if user has no token it will not work
-  def get(self, name):
-    #New method
-    item = next(filter(lambda x: x["name"] == name, items), None)
-    return {"item": item}, 200 if item else 404
-    #old method
-    # for item in items:
-    #   if item["name"] == name:
-    #        return item
-    
-  def post(self, name):
-    if next(filter(lambda x: x["name"] == name, items), None):
-      return {"message": "An item with the name '{}' already exist. ".format(name)}, 400
-    # data = request.get_json()
-    data = Item.parser.parse_args()
-    item = {"name": name, "price": data["price"]}
-    items.append(item) #append this new item, after you have append then return the item
-    return item, 201
-
-  def delete(self, name):
-    global items #we are calling the main items list from above
-    items = list(filter(lambda x: x["name"] != name, items)) #if name is not equal to that name return the remaining item list
-    return {"message": "item deleted successfully"}
-    
-  #WE USE THIS TO EDIT AND UPDATE OUR RECORDS
-  def put(self, name):
-    
-    # data = request.get_json()  #not using this cos of the new variable parser
-    data = Item.parser.parse_args()    #we use Item cos parser belongs to the class Item
-
-    item = next(filter(lambda x: x["name"] == name, items), None)
-    if item is None:
-      item = {"name": "name", "price": data["price"]}
-      items.append(item)
-    else:
-      item.update(data)
-    return item
-
-class ItemList(Resource):
-  def get(self):
-    return {"items": items}
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
 
-app.run(port=5000, debug=True)
+if __name__ =="__main__":
+    from db import db  # we do this here because of wht is called circular import
+    db.init_app(app)   # then in all our models we import the db
+    app.run(port=5000, debug=True)
